bc8ae437937e10fb2b046439b31a0a1119784d86
Dara Ouk <vdb.dara.ouk@gmail.com>
2017-09-25 19:17:07 +0700

    fixed migrate error and new clients table migration

---
+++ database/migrations/2017_09_25_120607_create_clients_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('company');
+            $table->string('phone', 24);
+            $table->string('email');
+            $table->integer('headcount');
+            $table->float('monthly_rate', 4, 2);
+            $table->float('monthly_value', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
