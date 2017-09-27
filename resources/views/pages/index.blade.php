22184ba38c9f1ea151e9f6470559e6c0e6008912
Dara Ouk <vdb.dara.ouk@gmail.com>
2017-09-27 21:53:30 +0700

    created form demo UI for add new customer section

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,60 @@
 
     <h2>Add New Customer</h2>
 
-    <h4>[Form Goes Here]</h4>
+    <form action="/">
+        <div class="container">
+            <!-- Row 1 -->
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="client-name">Record ID</label>
+                    <input type="text" class="form-control" id="client-id" placeholder="Auto Generate" disabled>
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="client-name">Client Name</label>
+                    <input type="text" class="form-control" id="client-name" placeholder="ex. John Smith">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="client-org">Organization</label>
+                    <input type="text" class="form-control" id="client-org" placeholder="ex. ABC Group Cambodia">
+                </div>
+                
+            </div>
+            <!-- Row 2 -->
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="client-phone">Phone Number</label>
+                    <input type="text" class="form-control" id="client-phone" placeholder="ex. (905) 365-7288">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="client-email">E-Mail Address</label>
+                    <input type="text" class="form-control" id="client-email" placeholder="ex. johnsmith@abcgroup.com">
+                </div>
+            </div>
+            <!-- Row 3 -->
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="client-hc">Employee Headcount</label>
+                    <input type="text" class="form-control" id="client-hc" placeholder="ex. 650">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="client-rate">Monthly Subscription Rate</label>
+                    <input type="number" class="form-control" min="0" step="0.25" id="client-rate" placeholder="ex. 5.00">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="client-value">Total Monthly Value</label>
+                    <input type="number" class="form-control" min="0" step="0.25" id="client-value" placeholder="ex. 3250">
+                </div>
+            </div>
+            <!-- Row 4 -->
+            <div class="row">
+                <div class="form-group col-md-12">
+                    <button type="reset" class="btn btn-default">Reset</button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </div>
+        </div>
+        
+    </form>
 
     <h2>View All Customers</h2>
     <div class="container">
